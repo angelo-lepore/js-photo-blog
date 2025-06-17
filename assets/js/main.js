5c50eb89165244f4a43f163a66ba3ab2f5136ab6
angelo-lepore <angelo.lepore94@gmail.com>
2025-06-17 15:36:14 +0200

    exercise-finished-and-bonus

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,23 @@
-const prova = "Pippo";
+const rowEl = document.querySelector(".row");
 
-console.log(prova);
+const endpoinUrl = "https://lanciweb.github.io/demo/api/pictures/";
+
+fetch(endpoinUrl)
+  .then((res) => res.json())
+  .then((data) => {
+    console.log(data);
+    data.forEach((post) => {
+      const { title, date, url } = post;
+      const postMarkupStr = `
+        <div class="col-4">
+             <div class="card">
+                 <img class="pin" src="./assets/img/pin.svg">
+                 <img class= "img" src="${url}">
+                 <p>${title}</p>
+                 <p>${date}</p>
+             </div>
+        </div>
+      `;
+      rowEl.insertAdjacentHTML("beforeend", postMarkupStr);
+    });
+  });
